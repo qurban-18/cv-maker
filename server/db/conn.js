4e785e9ab6014d83cbb4e36688b03be84b0da768
Qurban <malikqurbanawan807@gmail.com>
2021-09-22 18:44:33 +0500

    mongoDB key visible(fixed)

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,8 +1,6 @@
 import {} from "dotenv/config";
 import mongoose from "mongoose";
 
-// const URI = `mongodb+srv://qurban:123@cluster0.vxjpl.mongodb.net/CVmaker?retryWrites=true&w=majority`;
-
 const connection = mongoose
   .connect(process.env.DB_URI, {
     useNewUrlParser: true,
